58d684be8a687311a71d21ee5a6f7ee5ae7c6dac
iShoockx <160795305+iShoockx@users.noreply.github.com>
2024-04-12 21:17:19 -0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.btn');
-const container = document.querySelector('body');
-btn.onclick = function(){
-    this.classList.toggle('active')
-    container.classList.toggle('active')
-}
